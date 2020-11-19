6edc901521a397cea38c3330fd169ca9340e77b7
Caduzzin <73094512+Caduzzin@users.noreply.github.com>
2020-11-19 14:17:55 -0300

    19/11/2020 - Adding saving text in Docx

--- wikipediaSearch.py
+++ wikipediaSearch.py
@@ -1,6 +1,9 @@
 import wikipedia
 from wikipedia.exceptions import DisambiguationError
+import docx
+import asyncio
 
+doc = docx.Document()
 '''
     Author: Carlos Eduardo
     Last Update: 11/19/2020
@@ -8,12 +11,19 @@ from wikipedia.exceptions import DisambiguationError
 
 # --- Wikipedia Class to Search and Environment --- #
 class Wikipedia:
-    def search(search):                  
+
+    def search(search):
         try:
-            wikipedia.set_lang("pt")  
-            result = wikipedia.summary(wikipedia.search(str(search))[0])            
+            wikipedia.set_lang("pt")
+            result = wikipedia.summary(wikipedia.search(str(search))[0])
             return result
 
         except DisambiguationError as error: 
             print(error)
             pass
+
+
+# --- Saving text in docx archive --- #
+search = Wikipedia.search(input('Digite uma busca: '))
+paraObject = doc.add_paragraph("    " + search)
+doc.save('docx.docx')
